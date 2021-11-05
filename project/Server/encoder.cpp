ed3e36d82fb4a1adfd463adc779f139654807924
xiao Zhong <michaelzhong009@gmail.com>
2021-11-04 23:37:09 -0400

    Update encoder.cpp

--- project/Server/encoder.cpp
+++ project/Server/encoder.cpp
@@ -40,7 +40,7 @@ void handle_input(int argc, char* argv[], int* payload_size) {
 }
 //Xiao Zhong
 //djfkjdkfjkdjfkldjf
-//hello
+//hello Xiao
 int main(int argc, char* argv[]) {
 	stopwatch ethernet_timer;
 	unsigned char* input[NUM_PACKETS];
